35931f94ac938631090a371ed3199fd28ba340cb
Didas Mbalanya <didasmbalanya@gmail.com>
2020-05-17 01:15:48 +0200

    Updates

---
+++ precache-manifest.4179e8b493acea351ef9faeb57b36084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76542df01a72faa3dd2b5b03d163568",
+    "url": "/github.io/season-display/index.html"
+  },
+  {
+    "revision": "4e8b57a91adb55b0e203",
+    "url": "/github.io/season-display/static/css/main.f0b7f4ed.chunk.css"
+  },
+  {
+    "revision": "ec59a5f2b1cbb275de6b",
+    "url": "/github.io/season-display/static/js/2.e06a4b59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/season-display/static/js/2.e06a4b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8b57a91adb55b0e203",
+    "url": "/github.io/season-display/static/js/main.d9328516.chunk.js"
+  },
+  {
+    "revision": "47a562a0f4b6f6b1ab11",
+    "url": "/github.io/season-display/static/js/runtime-main.e137d0b1.js"
+  }
+]);
